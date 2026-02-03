6695281c55b12379956acd6ae894ddf734e676ac
Jorge Abalo <jabalo1988@gmail.com>
2026-02-03 15:25:14 +0100

    Curso de Angular

---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from "@angular/router";
+import { CountryPage } from "./country-page/country-page";
+
+
+
+export const countryRoutees: Routes = [
+  {
+    path: '',
+    component: CountryPage,
+  }
+]
---
+++ src/app/reactive/pages/dynamic-page/dynamic-page.ts
@@ -0,0 +1,31 @@
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, FormGroup, Validators, FormArray } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-dynamic-page',
+  imports: [JsonPipe, ReactiveFormsModule],
+  templateUrl: './dynamic-page.html',
+})
+export class DynamicPage {
+
+  private fb = inject(FormBuilder);
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array(
+      [
+        ['Metal Gear Solid', Validators.required],
+        ['Death Stranding', Validators.required],
+      ],
+      Validators.minLength(2)
+    ),
+
+  })
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray
+  }
+
+}
